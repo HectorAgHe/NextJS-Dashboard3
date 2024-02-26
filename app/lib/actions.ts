47908080f7cc10e9519e6b250b82cd12bd3eefba
HectorAgHe <aguilarhector440@gmail.com>
2024-02-25 23:55:20 -0600

    funcional

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,16 +7,33 @@ import { redirect } from 'next/navigation';
  
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    
+    amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
+
     date: z.string(),
   });
    
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+  export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+  };
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice( formData: FormData) {
     
     const { customerId, amount, status } = CreateInvoice.parse({    
       customerId: formData.get('customerId'),
@@ -62,7 +79,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
